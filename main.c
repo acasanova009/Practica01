b83efbbbce896cee098a1943d471100815a6d550
Alfonso Casanova <alfonso.casanova.009@outlook.com>
2021-09-10 21:24:34 -0500

    Gitignore added

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   int x = 7;
-   x=10;
-   x=x++;
-   int y=x;
+   
+   printf("Hello, World! %d",x);
    return 0;
 }
